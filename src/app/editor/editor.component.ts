266e5b8d5144c04ebbc5b953e043950e599f3067
rapteon <rapteon@tutanota.com>
2022-08-21 21:30:20 +0530

    Moved note.ts to "app" directory and wrote class for editor.

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Note} from '../note';
+
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
 
-  constructor() { }
+export class EditorComponent implements OnInit {
+  suggestionText: string = "## Heading\nWrite something…";
+  currentNote: Note | undefined;
+  currentView: "edit" | "preview";
+  text: string;
+  displayTogether: boolean;
+  onEdit(): void {
+    if (this.currentView === "preview") {
+      this.currentView = "edit";
+    }
+    else {
+      // Nothing to do.
+    }
+  }
+  onPreview(): void {
+    if (this.currentView === "edit") {
+      this.currentView = "preview";
+    }
+    else {
+      // Nothing to do.
+    }
+  }
+
+
+  constructor() {
+    this.currentView = "preview";
+    this.text = this.suggestionText;
+    this.displayTogether = true;
+  }
 
   ngOnInit(): void {
   }
