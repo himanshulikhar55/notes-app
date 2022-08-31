7cc32484814a3ccc3bbe4341be79cdacb298f6db
rapteon <rapteon@tutanota.com>
2022-08-31 21:20:20 +0530

    Modified mock notes to have custom dates.

--- src/app/mock-notes.ts
+++ src/app/mock-notes.ts
@@ -1,11 +1,11 @@
 import { Note } from './note';
 
 export const NOTES: Note[] = [
-  { id: 1, date: new Date(), content: '#### violet\na color' },
-  { id: 2, date: new Date(), content: '#### indigo\na color' },
-  { id: 3, date: new Date(), content: '#### blue\na color' },
-  { id: 4, date: new Date(), content: '#### green\na color' },
-  { id: 5, date: new Date(), content: '#### yellow\na color' },
-  { id: 6, date: new Date(), content: '#### orange\na color' },
-  { id: 7, date: new Date(), content: '#### red\na color' },
+  { id: 1, date: new Date('2022-08-17T03:24:00'), content: '#### violet\na color' },
+  { id: 2, date: new Date('2022-08-17T05:32:00'), content: '#### indigo\na color' },
+  { id: 3, date: new Date('2022-08-16T12:08:32'), content: '#### blue\na color' },
+  { id: 4, date: new Date('2022-08-14T13:32:58'), content: '#### green\na color' },
+  { id: 5, date: new Date('2022-08-13T02:02:45'), content: '#### yellow\na color' },
+  { id: 6, date: new Date('2022-08-11T15:55:46'), content: '#### orange\na color' },
+  { id: 7, date: new Date('2022-08-10T09:27:23'), content: '#### red\na color' },
 ];
